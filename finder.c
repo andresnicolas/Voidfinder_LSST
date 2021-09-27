5b004ce25f05f07b6b095881f71d2a176e875352
Martin de los Rios <martindelosrios13@gmail.com>
2021-09-27 11:41:54 +0200

    le pase cpplint a finder.c, limpie bastante pero hay un par de errores que no entiendo

--- finder.c
+++ finder.c
@@ -1,158 +1,189 @@
+/* Copyright [2021] Andrés Nicolas Ruiz */
 
+/* Include the directory when naming .h files */
 #include "global.h"
 #include "proto.h"
 
-void find_centers(float delta_seed, T_Healpix_Base<int> &hp, struct hpmap *map, vector <voids> &v)
-{
-
-  int i = 0;	
-  fix_arr <int,8> neigh;
+/* 
+Is this a non-const reference? If so, make const or use a
+  pointer: T_Healpix_Base<int> &hp 
+Is this a non-const reference? If so, make const or use a 
+  pointer: vector<voids> &v
+*/
+void find_centers(float delta_seed, T_Healpix_Base<int> &hp,
+                  struct hpmap *map, vector <voids> &v) {
+  int i = 0;
+  fix_arr < int, 8 > neigh;
   bool center;
-  for (int ipix=0; ipix<hp.Npix(); ipix++) {
-      if (!map[ipix].mask) continue;	  
+  for (int ipix = 0; ipix < hp.Npix(); ipix++) {
+      if (!map[ipix].mask) continue;
       if (map[ipix].delta_smooth < delta_seed) {
-	 center = true;     
-         hp.neighbors(ipix,neigh);
-	 for (int in=0; in<8; in++) {
+         center = true;
+         hp.neighbors(ipix, neigh);
+         for (int in = 0; in < 8; in++) {
              if (neigh[in] == -1) continue;
-	     if (!map[neigh[in]].mask) continue;
+             if (!map[neigh[in]].mask) continue;
              if (map[neigh[in]].delta_smooth < map[ipix].delta_smooth)
-	        center = false;	     
-	 }
-	 if (!center) continue;     
-     	 v.push_back(voids());
+                 center = false;
+         }
+         if (!center) continue;
+         v.push_back(voids());
          v[i].coord_init = hp.pix2ang(ipix);
-	 i++;
+         i++;
       }
   }
-
 }
 
-void find_voids(float delta_cut, T_Healpix_Base<int> &hp, struct hpmap *map, 
-		vector <tracer> &tr, vector <tracer> &ran, vector <voids> &v)
-{
-
-  struct sort *sort_gal,*sort_ran;	
-  vector <double> dist_gal,dist_ran;
+/*
+Is this a non-const reference? If so, make const or use a 
+   pointer: vector<tracer> &tr
+Is this a non-const reference? If so, make const or use a 
+   pointer: vector<tracer> &ran
+Is this a non-const reference? If so, make const or use a
+   pointer: vector<voids> &v 
+*/
+void find_voids(float delta_cut, T_Healpix_Base<int> &hp, struct hpmap *map,
+                vector <tracer> &tr, vector <tracer> &ran,
+                vector <voids> &v) {
+  struct sort *sort_gal, *sort_ran;
+  vector <double> dist_gal, dist_ran;
 
   float rmax = 10.0 * DEG2RAD;
+/* 
+ Using C-style cast.  Use static_cast<double>(...) instead 
+ [readability/casting]
+*/
   float reff = 2.0 / sqrt((double)hp.Npix());
   int nr = round(rmax/reff);
-  float norm = (float)ran.size() / (float)tr.size(); 
+/* 
+ Using C-style cast.  Use static_cast<double>(...) instead 
+ [readability/casting]
+*/
+  float norm = (float)ran.size() / (float)tr.size();
 
-  for (int iv=0; iv<v.size(); iv++) {
+  for (int iv = 0; iv < v.size(); iv++) {
+      double theta1 = v[iv].coord_init.theta;
+      double phi1 = v[iv].coord_init.phi;
 
-      double theta1 = v[iv].coord_init.theta;	  
-      double phi1 = v[iv].coord_init.phi;	  
-    
-      for (int ir=nr; ir>=0; ir--) { 
-
-	  float radius = reff * (float)(ir+1);    
-          rangeset pix = hp.query_disc(v[iv].coord_init,radius);
+      for (int ir = nr; ir >= 0; ir--) {
+/*
+Using C-style cast.  Use static_cast<float>(...) instead 
+ [readability/casting]
+*/
+          float radius = reff * (float)(ir+1);
+          rangeset pix = hp.query_disc(v[iv].coord_init, radius);
           vector <int> pixels_inside = pix.toVector();
 
-    	  int ntrac = 0;
-          int nrand = 0;  
-          for (int i=0; i<pixels_inside.size(); i++) {
-	      int ipix = pixels_inside[i];	  
-	      if (!map[ipix].mask) continue;	  
-	      ntrac += map[ipix].ntrac;
-              nrand += map[ipix].nrand;	      
-	  }
+          int ntrac = 0;
+          int nrand = 0;
+          for (int i = 0; i < pixels_inside.size(); i++) {
+              int ipix = pixels_inside[i];
+              if (!map[ipix].mask) continue;
+              ntrac += map[ipix].ntrac;
+              nrand += map[ipix].nrand;
+          }
+/*
+Using C-style cast.  Use static_cast<float>(...) instead 
+ [readability/casting]
+*/
+          float delta = norm * ((float)ntrac / (float)nrand) - 1.0;
 
-	  float delta = norm * ((float)ntrac / (float)nrand) - 1.0;
-
-	  if (delta < delta_cut) {
-
-	     float r1 = reff * (float)ir;    
-	     float r2 = reff * (float)(ir+1);
-	     rangeset pix1 = hp.query_disc(v[iv].coord_init,r1);
-	     rangeset pix2 = hp.query_disc_inclusive(v[iv].coord_init,r2,4);
-	     rangeset pixd = pix2.op_andnot(pix1);
+          if (delta < delta_cut) {
+/*
+Using C-style cast.  Use static_cast<float>(...) instead 
+ [readability/casting]
+*/
+             float r1 = reff * (float)ir;
+             float r2 = reff * (float)(ir+1);
+             rangeset pix1 = hp.query_disc(v[iv].coord_init, r1);
+             rangeset pix2 = hp.query_disc_inclusive(v[iv].coord_init, r2, 4);
+             rangeset pixd = pix2.op_andnot(pix1);
              vector <int> pixels_inside = pix1.toVector();
              vector <int> pixels_ring = pixd.toVector();
-    	  
-	     int ntrac = 0;
-             int nrand = 0;  
-             for (int i=0; i<pixels_inside.size(); i++) {
-	         int ipix = pixels_inside[i];	  
-	         if (!map[ipix].mask) continue;	  
-	         ntrac += map[ipix].ntrac;
-                 nrand += map[ipix].nrand;	      
-	     }
-    	  
-	     for (int i=0; i<pixels_ring.size(); i++) {
-	         int ipix = pixels_ring[i];
-	         if (!map[ipix].mask) continue;
-	        
-	         if (map[ipix].ntrac > 0) {
-	            for (int j=0; j<map[ipix].ntrac; j++) {
-	                int id = map[ipix].tracer[j];
-	                double theta2 = tr[id].coord.theta;	  
+             int ntrac = 0;
+             int nrand = 0;
+             for (int i = 0; i < pixels_inside.size(); i++) {
+                 int ipix = pixels_inside[i];
+                 if (!map[ipix].mask) continue;
+                 ntrac += map[ipix].ntrac;
+                 nrand += map[ipix].nrand;
+             }
+             for (int i = 0; i < pixels_ring.size(); i++) {
+                 int ipix = pixels_ring[i];
+                 if (!map[ipix].mask) continue;
+                 if (map[ipix].ntrac > 0) {
+                    for (int j = 0; j < map[ipix].ntrac; j++) {
+                        int id = map[ipix].tracer[j];
+                        double theta2 = tr[id].coord.theta;
                         double phi2 = tr[id].coord.phi;
-	                double dist = acos(cos(theta1) * cos(theta2) 
-	                            + sin(theta1) * sin(theta2) * cos(phi1 - phi2));
-	                dist_gal.push_back(dist);
-	            }
-	         }
+                        double dist = acos(cos(theta1) * cos(theta2)
+                                    + sin(theta1) * sin(theta2)
+                                    * cos(phi1 - phi2));
+                        dist_gal.push_back(dist);
+                   }
+                 }
 
-	         if (map[ipix].nrand > 0) {
-	            for (int j=0; j<map[ipix].nrand; j++) {
-	                int id = map[ipix].random[j];
-	                double theta2 = ran[id].coord.theta;	  
+                 if (map[ipix].nrand > 0) {
+                    for (int j = 0; j < map[ipix].nrand; j++) {
+                        int id = map[ipix].random[j];
+                        double theta2 = ran[id].coord.theta;
                         double phi2 = ran[id].coord.phi;
-	                double dist = acos(cos(theta1) * cos(theta2) 
-	                            + sin(theta1) * sin(theta2) * cos(phi1 - phi2));
-	                dist_ran.push_back(dist);
-	            }
-	         }
-	     }
+                        double dist = acos(cos(theta1) * cos(theta2)
+                                    + sin(theta1) * sin(theta2)
+                                    * cos(phi1 - phi2));
+                        dist_ran.push_back(dist);
+                    }
+                 }
+              }
 
-	     sort_gal = (struct sort *) malloc(dist_gal.size()*sizeof(struct sort));
-	     sort_ran = (struct sort *) malloc(dist_ran.size()*sizeof(struct sort));
-	     for (int k=0; k<dist_gal.size(); k++) {
-	         sort_gal[k].val = dist_gal[k];
-	         sort_gal[k].ord = k;	    
-	     }
-	     for (int k=0; k<dist_ran.size(); k++) {
-	         sort_ran[k].val = dist_ran[k];
-	         sort_ran[k].ord = k;	    
-	     }
-             QSort(sort_gal,0,dist_gal.size()-1);
-             QSort(sort_ran,0,dist_ran.size()-1);
+             sort_gal = (struct sort *) malloc(dist_gal.size()
+                        * sizeof(struct sort));
+             sort_ran = (struct sort *) malloc(dist_ran.size()
+                        * sizeof(struct sort));
+             for (int k = 0; k < dist_gal.size(); k++) {
+                 sort_gal[k].val = dist_gal[k];
+                 sort_gal[k].ord = k;
+             }
+             for (int k = 0; k < dist_ran.size(); k++) {
+                 sort_ran[k].val = dist_ran[k];
+                 sort_ran[k].ord = k;
+             }
+             QSort(sort_gal, 0, dist_gal.size()-1);
+             QSort(sort_ran, 0, dist_ran.size()-1);
 
-	     for (int k=0; k<dist_gal.size()-1; k++) {
-	         float radius = 0.5*(sort_gal[k].val + sort_gal[k+1].val);
-	         int kr = -1;
-	         for (int kk=0; kk<dist_ran.size(); kk++) { 
-	     	     if (sort_ran[kk].val < radius) 
-	     	        kr = kk; 
-	             else
-	     	        break;
-	         }	
-	         if (kr == -1) continue;
-	         
-		 int nt = ntrac + k + 1;
-		 int nr = nrand + kr + 1; 
-	         float delta = norm * ((float)nt / (float)nr) - 1.0;
-	    
-	         if (delta < delta_cut) {
-	            v[iv].radius = radius;
-	            v[iv].delta = delta;	       
-	            v[iv].tof = true;	    
-	         } 
-	     }
+             for (int k = 0; k < dist_gal.size()-1; k++) {
+                 float radius = 0.5*(sort_gal[k].val + sort_gal[k+1].val);
+                 int kr = -1;
+                 for (int kk = 0; kk < dist_ran.size(); kk++) {
+                     if (sort_ran[kk].val < radius)
+                        kr = kk;
+                     else
+                        break;
+                 }
+                 if (kr == -1) continue;
 
-	     dist_gal.clear();
-	     dist_ran.clear();
-	     free(sort_gal);
-	     free(sort_ran);
+                 int nt = ntrac + k + 1;
+                 int nr = nrand + kr + 1;
+                 float delta = norm * ((float)nt / (float)nr) - 1.0;
 
-	     break;
-	  }
+                 if (delta < delta_cut) {
+                    v[iv].radius = radius;
+                    v[iv].delta = delta;
+                    v[iv].tof = true;
+                 }
+             }
+
+             dist_gal.clear();
+             dist_ran.clear();
+             free(sort_gal);
+             free(sort_ran);
+
+             break;
+         }
       }
-      
-      if (v[iv].tof) fprintf(stdout,"%f %f %f \n",v[iv].radius*G.ShellDist,v[iv].coord_init.phi*RAD2DEG,90.0-v[iv].coord_init.theta*RAD2DEG);
-  }	  
 
+      if (v[iv].tof) fprintf(stdout, "%f %f %f \n",
+          v[iv].radius * G.ShellDist, v[iv].coord_init.phi*RAD2DEG,
+          90.0-v[iv].coord_init.theta*RAD2DEG);
+  }
 }
